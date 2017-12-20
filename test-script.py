85f882678356477eacc0b22af57dda746d72e25b
Kyle Briggs <kbrig035@uottawa.ca>
2017-12-20 14:51:15 -0500

    implemented correlation length calculation

--- test-script.py
+++ test-script.py
@@ -2,14 +2,15 @@ from ising2d import ising2d
 import numpy as np
 import pandas as pd
 import progressbar
+import itertools
 
 
 def main():
-    temperatures = np.linspace(1, 4, 20)
+    temperatures = np.linspace(2.3, 4, 20)
     fields= [0]
-    sizes = [10]
-    microstates = 10000
-    magnet = ising2d(temperatures, fields, sizes, microstates, algorithm = 'wolff', output_folder = 'output_wolff', verbose=True)
+    sizes = [40]
+    microstates = 1000
+    magnet = ising2d(temperatures, fields, sizes, microstates, algorithm = 'wolff', output_folder = 'output', verbose=True)
     magnet.run()
 
 if __name__=='__main__':
