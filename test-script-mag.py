4ccc5e196c102438223e978ade12fbf03f1a91be
Kyle Briggs <kbrig035@uottawa.ca>
2017-12-28 21:18:07 -0500

    tweaked B-field handling

--- test-script-mag.py
+++ test-script-mag.py
@@ -6,8 +6,8 @@ import itertools
 
 def main():
     temperatures = [2.0/np.log(1+np.sqrt(2))]
-    fields= np.array([10**n for n in np.linspace(-8, -4, 5)])
-    sizes = [128]
+    fields= np.array([10**n for n in np.linspace(-8, -1, 8)])
+    sizes = [64]
     microstates = 1000
     magnet = ising2d(temperatures, fields, sizes, microstates, algorithm = 'wolff', output_folder = 'B_output')
     magnet.run()
--- test-script.py
+++ test-script.py
@@ -7,7 +7,7 @@ import itertools
 def main():
     temperatures = [2.0/np.log(1+np.sqrt(2))]
     fields= [0]
-    sizes = [32]
+    sizes = [64, 50, 32, 23, 16]
     microstates = 1000
     magnet = ising2d(temperatures, fields, sizes, microstates, algorithm = 'wolff', output_folder = 'output_test')
     magnet.run()
